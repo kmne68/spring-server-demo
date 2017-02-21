22ab389f2ec00b76bcc25a4f6786f5e9e8f4537f
Keith Emery <kmne68@gmail.com>
2017-02-21 14:25:06 -0600

    Begin Hibernate form validation

--- src/com/kemery/spring/web/controllers/OffersController.java
+++ src/com/kemery/spring/web/controllers/OffersController.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -55,8 +58,21 @@ public class OffersController {
 	
 	
 	@RequestMapping(value="/docreate", method=RequestMethod.POST)
-	public String doCreate(Model model, Offer offer) {
+	public String doCreate(Model model, @Valid Offer offer, BindingResult result) {
 
+		if(result.hasErrors()) {
+			System.out.println("Form does not validate.");
+			
+			List<ObjectError> errors = result.getAllErrors();
+			
+			for(ObjectError error : errors) {
+				System.out.println(error.getDefaultMessage());
+			}
+			
+		} else {
+			System.out.println("Form validated");
+		}
+		
 		System.out.println(offer);
 		return "offerCreated";
 	}
